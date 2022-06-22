dd1792a2fa44d1307e812dfc11cfa1907de014bc
90ker <z1582373436@gmail.com>
2022-06-22 11:33:39 +0800

    feat: fill the frame

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,7 @@ watch(inputVal, async (newVal, oldVal) => {
     .then(buffer => new DataView(buffer))
     .then(dataView => {
       let idx = 0; // 表示当前指针所在字节位置
+      const len = dataView.byteLength;
       // 头 6byte
       let header = '';
       while (idx < 6) {
@@ -28,20 +29,103 @@ watch(inputVal, async (newVal, oldVal) => {
       }
       lsd.packageField = {
         globalColorTableFlag: parseInt(tmp[0], 2),
-        colorResolution:  parseInt(tmp.slice(1, 4), 2),
+        colorResolution: parseInt(tmp.slice(1, 4), 2),
         sortFlag: parseInt(tmp[4], 2),
         globalColorTableSize: parseInt(tmp.slice(5, 8), 2)
       }
       lsd.bgColorIndex = dataView.getUint8(idx++);
       lsd.pxAspectRadio = dataView.getUint8(idx++);
-      console.log(lsd);
-      
       if (lsd.packageField.globalColorTableFlag) {
-        // 加载全局色表
+        // TODO 加载全局色表
         idx += lsd.packageField.globalColorTableSize;
       }
 
-      let isExtend = dataView.getUint8(idx++); // 扩展
+      // 以下是循环
+
+      while (idx < len) {
+        let flagByte = dataView.getUint8(idx++);
+        if (flagByte === 33) { // 扩展
+          let type = dataView.getUint8(idx++);
+          switch (type) {
+            case 1: //纯文本
+              let textSize = dataView.getUint8(idx++);
+              idx += textSize;
+              break;
+            case 249: // 图像控制
+              let gce = {};
+              gce.byteSize = dataView.getUint8(idx++);
+              gce.packageField = dataView.getUint8(idx++);
+              let tmp = '';
+              for (let i = 8; i > 0; i--) {
+                tmp += (gce.packageField & 1 << i) >> i;
+              }
+              gce.packageField = {
+                unUse: parseInt(tmp.slice(0, 3), 2),
+                disposalMethod: parseInt(tmp.slice(3, 6), 2),
+                userInputFlag: parseInt(tmp[6], 2),
+                transparentColorFlag: parseInt(tmp[7], 2)
+              }
+              gce.delayTime = dataView.getUint16(idx);
+              idx += 2;
+              gce.transparentColorIndex = dataView.getUint8(idx++);
+              break;
+            case 254: // 评论
+              let commentEOF = dataView.getUint8(idx++);
+              break;
+            case 255: //应用
+              let appSize = dataView.getUint8(idx++);
+              let flag = '';
+              while (appSize-- > 3) {
+                flag += String.fromCharCode(dataView.getUint8(idx++));
+              }
+              let captcha = '';
+              while (appSize-- > 0) {
+                captcha += String.fromCharCode(dataView.getUint8(idx++));
+              }
+              break;
+          }
+          let EOF = dataView.getUint8(idx++);
+        } else if (flagByte === 44) {
+          let id = {}; // 图像描述
+          id.left = dataView.getUint16(idx);
+          idx += 2;
+          id.right = dataView.getUint16(idx);
+          idx += 2;
+          id.width = dataView.getUint16(idx);
+          idx += 2;
+          id.height = dataView.getUint16(idx);
+          idx += 2;
+          id.packageField = dataView.getUint8(idx++);
+          // 感觉比较笨的方法 byte -> 8bit
+          let tmp2 = '';
+          for (let i = 8; i > 0; i--) {
+            tmp2 += (id.packageField & 1 << i) >> i;
+          }
+          id.packageField = {
+            localColorTableFlag: parseInt(tmp2[0], 2),
+            interlaceFlag: parseInt(tmp2[1], 2),
+            sortFlag: parseInt(tmp2[2], 2),
+            unUse: parseInt(tmp2.slice(3, 5), 2),
+            localColorTableSize: parseInt(tmp2.slice(5, 8), 2)
+          }
+          if (id.packageField.localColorTableFlag) {
+            // TODO 加载局部色表
+            idx += id.packageField.localColorTableSize;
+          }
+          // 加载数据
+          let minCodeSizeLZW = dataView.getUint8(idx++);
+          let subBlockSize = dataView.getUint8(idx++);
+          idx += subBlockSize;
+          let flag = dataView.getUint8(idx++);
+          let blocks = 1;
+          while (flag) {
+            blocks ++;
+            // TODO 解析数据块
+            idx += subBlockSize;
+            flag = dataView.getUint8(idx++);
+          }
+        }
+      }
 
     })
 }, { immediate: true })
